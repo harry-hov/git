283d2c01365d39d775065c0f0827c551e72ee601
Hariom Verma <hariom18599@gmail.com>
2020-06-18 16:59:19 +0530

    ref-filter: add `sanitize` option in `subject`
    Currently, subject does not take any arguments. This commit introduced
    `sanitize` formatting option for `subject`.
    
    `subject:sanitize` - print sanitized subject line, suitable for a filename.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Hariom Verma <hariom18599@gmail.com>

--- pretty.c
+++ pretty.c
@@ -12,6 +12,7 @@
 #include "reflog-walk.h"
 #include "gpg-interface.h"
 #include "trailer.h"
+#include "format-support.h"
 
 static char *user_format;
 static struct cmt_fmt_map {
@@ -831,40 +832,6 @@ static void parse_commit_header(struct format_commit_context *context)
 	}
 	context->message_off = i;
 	context->commit_header_parsed = 1;
-}
-
-static int istitlechar(char c)
-{
-	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
-		(c >= '0' && c <= '9') || c == '.' || c == '_';
-}
-
-static void format_sanitized_subject(struct strbuf *sb, const char *msg)
-{
-	size_t trimlen;
-	size_t start_len = sb->len;
-	int space = 2;
-
-	for (; *msg && *msg != '\n'; msg++) {
-		if (istitlechar(*msg)) {
-			if (space == 1)
-				strbuf_addch(sb, '-');
-			space = 0;
-			strbuf_addch(sb, *msg);
-			if (*msg == '.')
-				while (*(msg+1) == '.')
-					msg++;
-		} else
-			space |= 1;
-	}
-
-	/* trim any trailing '.' or '-' characters */
-	trimlen = 0;
-	while (sb->len - trimlen > start_len &&
-		(sb->buf[sb->len - 1 - trimlen] == '.'
-		|| sb->buf[sb->len - 1 - trimlen] == '-'))
-		trimlen++;
-	strbuf_remove(sb, sb->len - trimlen, trimlen);
 }
 
 const char *format_subject(struct strbuf *sb, const char *msg,
