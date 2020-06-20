7dfcd4f8f4bffaf8c9ac5b258098b61efc4d34f1
Hariom Verma <hariom18599@gmail.com>
2020-06-20 16:34:32 +0530

    pretty-lib: add formatting option `m`, `d`, and `D`
    Add 3 more formatting option from pretty.c to pretty-lib.c
    Not using ref-filters logic.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Hariom Verma <hariom18599@gmail.com>

--- pretty-lib.c
+++ pretty-lib.c
@@ -37,6 +37,15 @@ static size_t convert_format(struct strbuf *sb, const char *start, void *data)
 	case 'p':
 		strbuf_addstr(sb, "%(parent:short)");
 		return 1;
+	case 'm':		/* left/right/bottom */
+		strbuf_addstr(sb, get_revision_mark(NULL, c->commit));
+		return 1;
+	case 'd':
+		format_decorations(sb, c->commit, c->auto_color);
+		return 1;
+	case 'D':
+		format_decorations_extended(sb, c->commit, c->auto_color, "", ", ", "");
+		return 1;
 	case 'a':
 		if (start[1] == 'n')
 			strbuf_addstr(sb, "%(authorname)");
