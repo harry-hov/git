051eed037a6a38e025db9ce7c7054da7d90d999e
Hariom Verma <hariom18599@gmail.com>
2020-07-05 17:47:49 +0530

    pretty-lib: add color related formatting options
    Add color related formatting options like C(<color>), C<color> with
    `auto` and `always` support.
    
    Not using ref-filters logic.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Hariom Verma <hariom18599@gmail.com>

--- pretty-lib.c
+++ pretty-lib.c
@@ -1,16 +1,29 @@
 #include "commit.h"
 #include "ref-filter.h"
 #include "pretty-lib.h"
+#include "color.h"
+#include "pretty.h"
+#include "diff.h"
+#include "log-tree.h"
+#include "format-support.h"
 
 static size_t convert_format(struct strbuf *sb, const char *start, void *data)
 {
+	struct format_commit_context *c = data;
+
 	/* TODO - Add support for more formatting options */
 	switch (*start) {
+	case 'C':
+		return format_commit_color(sb, start, c);
 	case 'H':
+		strbuf_addstr(sb, diff_get_color(c->auto_color, DIFF_COMMIT));
 		strbuf_addstr(sb, "%(objectname)");
+		strbuf_addstr(sb, diff_get_color(c->auto_color, DIFF_RESET));
 		return 1;
 	case 'h':
+		strbuf_addstr(sb, diff_get_color(c->auto_color, DIFF_COMMIT));
 		strbuf_addstr(sb, "%(objectname:short)");
+		strbuf_addstr(sb, diff_get_color(c->auto_color, DIFF_RESET));
 		return 1;
 	case 'T':
 		strbuf_addstr(sb, "%(tree)");
@@ -71,11 +84,16 @@ void ref_pretty_print_commit(struct pretty_print_context *pp,
 {
 	struct ref_format format = REF_FORMAT_INIT;
 	struct strbuf sb_fmt = STRBUF_INIT;
+	struct format_commit_context fmt_ctx = {
+		.commit = commit,
+		.pretty_ctx = pp,
+		.wrap_start = sb->len
+	};
 	const char *name = "refs";
 	const char *usr_fmt = get_user_format();
 
 	if (pp->fmt == CMIT_FMT_USERFORMAT) {
-		strbuf_expand(&sb_fmt, usr_fmt, convert_format, NULL);
+		strbuf_expand(&sb_fmt, usr_fmt, convert_format, &fmt_ctx);
 		format.format = sb_fmt.buf;
 	} else if (pp->fmt == CMIT_FMT_DEFAULT || pp->fmt == CMIT_FMT_MEDIUM) {
 		format.format = "Author: %(authorname) %(authoremail)\nDate:\t%(authordate)\n\n%(subject)\n\n%(body)";
