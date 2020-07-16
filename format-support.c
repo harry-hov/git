4571be824e1759df2e576bbd3f3a4a70f4c2ed53
Hariom Verma <hariom18599@gmail.com>
2020-07-16 17:01:25 +0530

    format-support: move `color` logic from pretty.c
    Declared `struct format_commit_context` and parse_color() in format-support.h
    As we might need to use this in pretty-lib.c
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Hariom Verma <hariom18599@gmail.com>

--- format-support.c
+++ format-support.c
@@ -36,3 +36,55 @@ void format_sanitized_subject(struct strbuf *sb, const char *msg)
 		trimlen++;
 	strbuf_remove(sb, sb->len - trimlen, trimlen);
 }
+
+size_t pretty_parse_color(struct strbuf *sb, /* in UTF-8 */
+			  const char *placeholder,
+			  struct format_commit_context *c)
+{
+	const char *rest = placeholder;
+	const char *basic_color = NULL;
+
+	if (placeholder[1] == '(') {
+		const char *begin = placeholder + 2;
+		const char *end = strchr(begin, ')');
+		char color[COLOR_MAXLEN];
+
+		if (!end)
+			return 0;
+
+		if (skip_prefix(begin, "auto,", &begin)) {
+			if (!want_color(c->pretty_ctx->color))
+				return end - placeholder + 1;
+		} else if (skip_prefix(begin, "always,", &begin)) {
+			/* nothing to do; we do not respect want_color at all */
+		} else {
+			/* the default is the same as "auto" */
+			if (!want_color(c->pretty_ctx->color))
+				return end - placeholder + 1;
+		}
+
+		if (color_parse_mem(begin, end - begin, color) < 0)
+			die(_("unable to parse --pretty format"));
+		strbuf_addstr(sb, color);
+		return end - placeholder + 1;
+	}
+
+	/*
+	 * We handle things like "%C(red)" above; for historical reasons, there
+	 * are a few colors that can be specified without parentheses (and
+	 * they cannot support things like "auto" or "always" at all).
+	 */
+	if (skip_prefix(placeholder + 1, "red", &rest))
+		basic_color = GIT_COLOR_RED;
+	else if (skip_prefix(placeholder + 1, "green", &rest))
+		basic_color = GIT_COLOR_GREEN;
+	else if (skip_prefix(placeholder + 1, "blue", &rest))
+		basic_color = GIT_COLOR_BLUE;
+	else if (skip_prefix(placeholder + 1, "reset", &rest))
+		basic_color = GIT_COLOR_RESET;
+
+	if (basic_color && want_color(c->pretty_ctx->color))
+		strbuf_addstr(sb, basic_color);
+
+	return rest - placeholder;
+}
