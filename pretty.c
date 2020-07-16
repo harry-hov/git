30716b0ad0e49b27c4d45578fb9d1cd60fc5cffc
Hariom Verma <hariom18599@gmail.com>
2020-07-16 22:55:08 +0530

    format-support: move `pretty_switch_line_wrapping()` from pretty.c
    Move `pretty_switch_line_wrapping()` and all the function it uses from
    pretty.c to format-support.c
    
    As later we plan to use same logic in pretty-lib.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Hariom Verma <hariom18599@gmail.com>

--- pretty.c
+++ pretty.c
@@ -739,35 +739,6 @@ static void parse_commit_message(struct format_commit_context *c)
 	c->commit_message_parsed = 1;
 }
 
-static void strbuf_wrap(struct strbuf *sb, size_t pos,
-			size_t width, size_t indent1, size_t indent2)
-{
-	struct strbuf tmp = STRBUF_INIT;
-
-	if (pos)
-		strbuf_add(&tmp, sb->buf, pos);
-	strbuf_add_wrapped_text(&tmp, sb->buf + pos,
-				(int) indent1, (int) indent2, (int) width);
-	strbuf_swap(&tmp, sb);
-	strbuf_release(&tmp);
-}
-
-static void rewrap_message_tail(struct strbuf *sb,
-				struct format_commit_context *c,
-				size_t new_width, size_t new_indent1,
-				size_t new_indent2)
-{
-	if (c->width == new_width && c->indent1 == new_indent1 &&
-	    c->indent2 == new_indent2)
-		return;
-	if (c->wrap_start < sb->len)
-		strbuf_wrap(sb, c->wrap_start, c->width, c->indent1, c->indent2);
-	c->wrap_start = sb->len;
-	c->width = new_width;
-	c->indent1 = new_indent1;
-	c->indent2 = new_indent2;
-}
-
 static int match_placeholder_arg_value(const char *to_parse, const char *candidate,
 				       const char **end, const char **valuestart,
 				       size_t *valuelen)
@@ -838,34 +809,6 @@ static int format_trailer_match_cb(const struct strbuf *key, void *ud)
 			return 1;
 	}
 	return 0;
-}
-
-static int pretty_switch_line_wrapping(struct strbuf *sb, const char *placeholder,
-				       struct format_commit_context *c)
-{
-	if (placeholder[1] == '(') {
-		unsigned long width = 0, indent1 = 0, indent2 = 0;
-		char *next;
-		const char *start = placeholder + 2;
-		const char *end = strchr(start, ')');
-		if (!end)
-			return 0;
-		if (end > start) {
-			width = strtoul(start, &next, 10);
-			if (*next == ',') {
-				indent1 = strtoul(next + 1, &next, 10);
-				if (*next == ',') {
-					indent2 = strtoul(next + 1,
-								&next, 10);
-				}
-			}
-			if (*next != ')')
-				return 0;
-		}
-		rewrap_message_tail(sb, c, width, indent1, indent2);
-		return end - placeholder + 1;
-	} else
-		return 0;
 }
 
 static size_t format_commit_one(struct strbuf *sb, /* in UTF-8 */
