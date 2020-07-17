e38c0b85f31051219928a4e73e4eef0b4416fb02
Hariom Verma <hariom18599@gmail.com>
2020-07-17 20:13:24 +0530

    ref_format: add option to skip `\n` at eol
    We might always need a `\n` at end of line in the case of
    `git for-each-ref`. But as we intend to use ref-filter's logic
    in pretty, having an option to skip `\n` will be useful.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Hariom Verma <hariom18599@gmail.com>

--- ref-filter.h
+++ ref-filter.h
@@ -76,11 +76,13 @@ struct ref_format {
 	int quote_style;
 	int use_color;
 
+	unsigned int need_newline_at_eol : 1;
+
 	/* Internal state to ref-filter */
 	int need_color_reset_at_eol;
 };
 
-#define REF_FORMAT_INIT { NULL, 0, -1 }
+#define REF_FORMAT_INIT { NULL, 0, -1, 1 }
 
 /*  Macros for checking --merged and --no-merged options */
 #define _OPT_MERGED_NO_MERGED(option, filter, h) \
