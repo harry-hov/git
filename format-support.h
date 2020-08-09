003c81e8eaedeb2e39c5ea92115fe38e0bfd02f8
Hariom Verma <hariom18599@gmail.com>
2020-08-09 13:56:20 +0530

    format-support: move `format_set_trailers_options()` from pretty
    This is another step to make it possible to reuse trailer related
    functions in ref-filter.c.
    
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Heba Waly <heba.waly@gmail.com>
    Signed-off-by: Hariom Verma <hariom18599@gmail.com>

--- format-support.h
+++ format-support.h
@@ -1,6 +1,14 @@
 #ifndef FORMAT_SUPPORT_H
 #define FORMAT_SUPPORT_H
 
+struct process_trailer_options;
+struct string_list;
+
 void format_sanitized_subject(struct strbuf *sb, const char *msg, size_t len);
+
+int format_set_trailers_options(struct process_trailer_options *opts,
+				struct string_list *filter_list,
+				struct strbuf *sepbuf,
+				const char **arg);
 
 #endif /* FORMAT_SUPPORT_H */
